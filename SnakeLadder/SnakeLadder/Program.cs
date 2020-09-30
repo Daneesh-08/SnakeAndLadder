be146cec7740154680996b3289b498ee2ade4e4c
Daneesh <mustafadaneesh@gmail.com>
2020-09-30 20:24:52 +0530

    [Daneesh] Refactor : Rolling the Die

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -8,6 +8,9 @@ namespace SnakeLadder
         {
             int pos = 0;
             Console.WriteLine("Position :  " + pos);
+            Random random = new Random();
+            int dieNum = random.Next(1,7);
+            Console.WriteLine("Number after rolling die :  " + dieNum);
         }
     }
 }
