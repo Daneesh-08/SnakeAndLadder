349ba3afb7792cc1270289e601d88090dc1a4932
Daneesh <mustafadaneesh@gmail.com>
2020-09-30 20:09:05 +0530

    [Daneesh] Refactor : Single Player Start Position

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0;
+            Console.WriteLine("Position :  " + pos);
+        }
+    }
+}
